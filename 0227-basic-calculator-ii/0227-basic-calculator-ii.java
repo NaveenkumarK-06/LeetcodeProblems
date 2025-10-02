bf10432d3353a623127564d1c0fe4d691cbc5c10
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-10-02 20:29:52 +0530

    Time: 26 ms (27.94%), Space: 46.4 MB (53.09%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,36 +1,30 @@
 class Solution {
     public int calculate(String s) {
-        int result = 0;
-        int lastNum = 0;  // stores last number for * or /
-        int num = 0;
-        char prevOperator = '+';
+        Stack<Integer> stack=new Stack<>();
+        int num=0;
+        char prevOperator='+';
 
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+        for(int i=0;i<=s.length();i++){
+            char ch=(i<s.length())?s.charAt(i):'\0';
 
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
+            if(Character.isDigit(ch)){
+                num=num*10+(ch-'0');
             }
 
-            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
-                if (prevOperator == '+') {
-                    result += lastNum;  // add previous number to result
-                    lastNum = num;      // update lastNum
-                } else if (prevOperator == '-') {
-                    result += lastNum;
-                    lastNum = -num;
-                } else if (prevOperator == '*') {
-                    lastNum = lastNum * num;
-                } else if (prevOperator == '/') {
-                    lastNum = lastNum / num;
-                }
+            if(!Character.isDigit(ch) && ch!=' ' || i==s.length()){
+                if(prevOperator =='+')  stack.push(num);
+                if(prevOperator =='-')  stack.push(-num);
+                if(prevOperator =='*')  stack.push(stack.pop()*num);
+                if(prevOperator =='/')  stack.push(stack.pop()/num);
 
-                prevOperator = ch;
-                num = 0;
+                prevOperator =ch;
+                num=0;
             }
         }
-
-        result += lastNum; // add the last pending number
-        return result;
+        int ans=0;
+        while(!stack.isEmpty()){
+            ans+=stack.pop();
+        }
+        return ans;
     }
 }
