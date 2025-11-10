b24c26ba896641d5576c17a7a7ff95b1c563cb0c
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-11-10 21:39:41 +0530

    Time: 33 ms (13.82%), Space: 50.3 MB (5.67%) - LeetHub

--- 0697-degree-of-an-array/0697-degree-of-an-array.java
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -1,31 +1,32 @@
 class Solution {
     public int findShortestSubArray(int[] nums) {
-        int degree=0;
-        int minLength=nums.length;
-        boolean visited[]=new boolean[nums.length];
+        int degree = 0;
+        int minLength = nums.length;
+        boolean visited[] = new boolean[nums.length];
 
-        for(int i=0;i<nums.length;i++){
-            int count =0;
-            int start=i;
-            int end=i;
-            if(visited[i])continue;
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i]) continue;
 
-            for(int j=0;j<nums.length;j++){
-                if(nums[i]==nums[j]){
+            int count = 0;
+            int start = i;
+            int end = i;
+
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
                     count++;
-                    visited[j]=true;
-                    end=j;
+                    end = j;         // track last occurrence
+                    visited[j] = true;  // mark as visited
                 }
             }
-            if(count>degree){
-                degree=count;
-                minLength=end-start+1;
-            }
-            else if(count==degree){
-                minLength=Math.min(minLength,end-start+1);
-            }
 
+            if (count > degree) {
+                degree = count;                // new highest frequency found
+                minLength = end - start + 1;   // update subarray length
+            } else if (count == degree) {
+                minLength = Math.min(minLength, end - start + 1);
+            }
         }
+
         return minLength;
     }
 }
