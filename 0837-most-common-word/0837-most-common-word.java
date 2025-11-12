2b9f4137c3d5ed8a4b03df47b733599fb53b3500
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-11-12 16:01:20 +0530

    Time: 13 ms (80.17%), Space: 44.3 MB (15.34%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.java
+++ 0837-most-common-word/0837-most-common-word.java
@@ -1,45 +1,35 @@
+import java.util.*;
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
 
+        StringBuilder sb = new StringBuilder();
         paragraph = paragraph.toLowerCase();
-        String cleaned = "";
-        for (int i = 0; i < paragraph.length(); i++) {
-            char c = paragraph.charAt(i);
-            if ((c >= 'a' && c <= 'z') || c == ' ') {
-                cleaned += c;
+        for (char c : paragraph.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                sb.append(c);
             } else {
-                cleaned += " ";
+                sb.append(' ');
             }
         }
 
-        String[] words = cleaned.split("\\s+");
-        boolean[] visited = new boolean[words.length];
-        int maxCount = 0;
+        String[] words = sb.toString().split("\\s+");
+
+        Set<String> bannedSet = new HashSet<>(Arrays.asList(banned));
+
+        Map<String, Integer> freq = new HashMap<>();
+        for (String w : words) {
+            if (!bannedSet.contains(w)) {
+                freq.put(w, freq.getOrDefault(w, 0) + 1);
+            }
+        }
+
         String result = "";
-
-        for (int i = 0; i < words.length; i++) {
-            if (visited[i] || words[i].equals("")) continue;
-
-            boolean isBanned = false;
-            for (int b = 0; b < banned.length; b++) {
-                if (words[i].equals(banned[b])) {
-                    isBanned = true;
-                    break;
-                }
-            }
-            if (isBanned) continue;
-
-            int count = 1;
-            for (int j = i + 1; j < words.length; j++) {
-                if (words[i].equals(words[j])) {
-                    count++;
-                    visited[j] = true;
-                }
-            }
-
-            if (count > maxCount) {
-                maxCount = count;
-                result = words[i];
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                result = entry.getKey();
             }
         }
 
