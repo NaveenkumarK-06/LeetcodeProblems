0728047403f640ac9eb369e69c37bb1257ab34c6
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-11-24 18:10:49 +0530

    Time: 1 ms (99.62%), Space: 46.5 MB (18.7%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.java
@@ -1,19 +1,17 @@
 class Solution {
     public boolean canBeEqual(int[] target, int[] arr) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
 
-        for(int x : target){
-            hm.put(x,hm.getOrDefault(x,0)+1);
-        }
-        for(int y : arr){
-            if(!hm.containsKey(y)) return false;
-            hm.put(y,hm.get(y)-1);
-        }
-        for(int val : hm.values()){
-            if(val!=0){
+        int[] freq = new int[1001];
+
+        for(int x : target)
+            freq[x]++;
+
+        for(int y : arr)
+            freq[y]--;
+
+        for(int count : freq)
+            if(count != 0)
                 return false;
-            }
-        }
 
         return true;
     }
