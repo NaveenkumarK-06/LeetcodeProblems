c2c1f57bd8fb81e2c2384a182214a47ea2b470c3
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-11-26 14:57:22 +0530

    Time: 301 ms (70.4%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,3 +1,1 @@
-SELECT class FROM (SELECT class,count(*) AS freq 
-                  FROM Courses 
-                  GROUP BY class HAVING freq) AS t WHERE t.freq>=5;
+SELECT class FROM Courses GROUP BY class HAVING count(*)>=5;
