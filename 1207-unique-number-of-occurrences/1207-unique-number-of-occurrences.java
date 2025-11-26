9345e57c737c23bdf766fe36e9854d27e16678aa
NaveenkumarK-06 <naveenkumar.kothapalli06@gmail.com>
2025-11-26 21:22:56 +0530

    Time: 2 ms (98.24%), Space: 43.5 MB (30.1%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,20 +1,10 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        boolean isvisited[]=new boolean[arr.length];
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            if(isvisited[i]) continue;
-            int count=0;
-            for(int j=0;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                    isvisited[j]=true;
-                    
-                }
-            }
-            list.add(count);
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        for(int num : arr){
+            hm.put(num,hm.getOrDefault(num,0)+1);
         }
-         Set<Integer> set = new HashSet<>(list);
-         return list.size()==set.size();
+         Set<Integer> set = new HashSet<>(hm.values());
+         return hm.size()==set.size();
     }
 }
